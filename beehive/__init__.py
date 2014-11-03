5680bff8c8396c39f50e178833503f8a3ee5f6b5
Vadim Rutkovsky <vrutkovs@redhat.com>
2014-11-03 12:11:34 +0100

    __init__: add 'behave' module alias if behave is not available
    Fixes #11

--- beehive/__init__.py
+++ beehive/__init__.py
@@ -18,8 +18,13 @@ To get started, we recommend the `tutorial`_ and then the `test language`_ and
 
 __version__ = '0.9.1'
 
+import sys
 from beehive.step_registry import given, when, then, step, Given, When, Then, Step
 from beehive.matchers import use_step_matcher, step_matcher, register_type
 
 __step_names = (given, when, then, step, Given, When, Then, Step)
 __all__ = __step_names + (use_step_matcher, step_matcher, register_type)
+
+if 'behave' not in sys.modules:
+    # Add behave replacements
+    sys.modules['behave'] = sys.modules['beehive']
