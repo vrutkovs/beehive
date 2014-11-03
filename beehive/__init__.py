9b2fec91e8007982c43ce41df768e9958d259bd7
Vadim Rutkovsky <vrutkovs@redhat.com>
2014-11-03 13:29:49 +0100

    Release 1.0

--- beehive/__init__.py
+++ beehive/__init__.py
@@ -16,7 +16,7 @@ To get started, we recommend the `tutorial`_ and then the `test language`_ and
 .. _`api`: api.html
 '''
 
-__version__ = '0.9.1'
+__version__ = '1.0'
 
 import sys
 from beehive.step_registry import given, when, then, step, Given, When, Then, Step
