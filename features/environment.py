0e25f2ce7cd769f55dd7915e26957346c6eb8ad0
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-01-15 15:51:05 +0100

    Revert tests modification

--- features/environment.py
+++ features/environment.py
@@ -5,29 +5,8 @@
 # HOOKS:
 # -----------------------------------------------------------------------------
 def before_all(context):
-    #setup_context_with_global_params_test(context)
-    print("before_all")
+    setup_context_with_global_params_test(context)
 
-def after_all(context):
-    print("after_all")
-
-def before_feature(context, feature):
-    print("before_feature")
-
-def after_feature(context, feature):
-    print("after_feature")
-
-def before_scenario(context, scenario):
-    print("before_scenario")
-
-def after_scenario(context, scenario):
-    print("after_scenario")
-
-def before_step(context, step):
-    print("before_step")
-
-def after_step(context, step):
-    print("after_step")
 
 # -----------------------------------------------------------------------------
 # SPECIFIC FUNCTIONALITY:
